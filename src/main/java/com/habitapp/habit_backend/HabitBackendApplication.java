03815645ddbaf0a0f8eb980260b85408019926ab
Killian Portier <killian.portier@yahoo.fr>
2025-07-10 08:50:55 +0200

    Add neon db on heroku

--- src/main/java/com/habitapp/habit_backend/HabitBackendApplication.java
+++ src/main/java/com/habitapp/habit_backend/HabitBackendApplication.java
@@ -1,5 +1,6 @@
 package com.habitapp.habit_backend;
 
+import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableScheduling;
@@ -9,7 +10,11 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 public class HabitBackendApplication {
 
 	public static void main(String[] args) {
+		// Charge les variables .env dans les propriétés système
+		Dotenv dotenv = Dotenv.configure().ignoreIfMissing().load();
+
+		dotenv.entries().forEach(entry -> System.setProperty(entry.getKey(), entry.getValue()));
+
 		SpringApplication.run(HabitBackendApplication.class, args);
 	}
-
 }
